04ad2e0a9b23167fa192ffe7d154a67486e74a9d
Marc Edison <suanieeee@yahoo.com>
2024-08-01 02:27:20 +0800

    feat: Add Menu component for navigation

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,19 @@
 import Header from '@/components/Header'
 import './css/Home.css'
 import BottomNavs from '@/components/BottomNavs'
+import Menu from '@/components/Menu'
+import { useState } from 'react'
 
 export default function Home() {
+    const [isMenu, setMenu] = useState<boolean>(false)
+
     return (
         <>
             <div className="home w-full h-screen flex flex-col justify-start items-start">
-                <Header />
+                <Header isMenu={isMenu} menu={(e) => setMenu(e)} />
+                {
+                    isMenu && <Menu />
+                }
                 <BottomNavs />
             </div>
         </>
