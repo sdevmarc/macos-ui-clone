e52e65880146d9e67d3e5cbe7995711339505ff2
sdevmarc <suanieeee@yahoo.com>
2024-07-31 20:17:58 +0800

    Add Vite configuration and necessary files for the client-side code

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/Header'
+import './css/Home.css'
+
+export default function Home() {
+    return (
+        <>
+            <div className="home w-full h-screen flex flex-col justify-start items-start">
+                <Header />
+            </div>
+        </>
+    )
+}
