4e7c1582d2399840c93a82bddc2a573d35e61849
Marc Edison <suanieeee@yahoo.com>
2024-08-01 01:20:35 +0800

    feat: Add BottomNavs component for dock navigation

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Header from '@/components/Header'
 import './css/Home.css'
+import BottomNavs from '@/components/BottomNavs'
 
 export default function Home() {
     return (
         <>
             <div className="home w-full h-screen flex flex-col justify-start items-start">
                 <Header />
+                <BottomNavs />
             </div>
         </>
     )
